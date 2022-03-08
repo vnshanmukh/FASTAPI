9cbdd05feb2b41286294173dc08a5113f09cc309
vnshanmukh <vnshanmukh@gmail.com>
2022-03-08 17:50:21 +0530

    alembic post table creation

---
+++ alembic/versions/1fe78c18b9cf_create_post_table.py
@@ -0,0 +1,24 @@
+"""create_post_table
+
+Revision ID: 1fe78c18b9cf
+Revises: 
+Create Date: 2022-03-08 17:49:39.151866
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1fe78c18b9cf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
