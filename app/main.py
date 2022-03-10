6b74f21fe1087eb9ce2c7f651cccdd03d93e00df
vnshanmukh <vnshanmukh@gmail.com>
2022-03-10 12:16:52 +0530

    clean up

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+from ensurepip import version
 from fastapi import Depends, FastAPI
 import routers.post,routers.user,routers.auth,routers.vote
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 #models.Base.metadata.create_all(bind=database.engine)
-app = FastAPI()
+version = 'v1.0.0'
+description = """API for social media app"""
+app = FastAPI(
+    title= "MEDIA API",
+    version =version,
+    description  = description
+)
 favicon_path = 'favicon.ico'
 origins = ["*"]
 app.add_middleware(
@@ -16,6 +23,15 @@ app.add_middleware(
 @app.get('/favicon.ico',include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
+
+@app.get('/')
+async def home():
+    return {
+        "title": "FASTAPI",
+        "version" : version,
+        "description" : description,
+        "documentation" : "http://143.244.143.151/docs"
+    }
 
 app.include_router(routers.post.router)
 app.include_router(routers.user.router)
